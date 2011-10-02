c9e1127348d957c4c2a6bbdbb3139c5afd0d6659
cesine <cesine@host>
2011-10-02 11:28:33 -0400

    video recording making multiple files, turns on on begining and end of activity but also responds to touch

--- src/ca/ilanguage/oprime/bilingualaphasiatest/ui/VideoRecorderSubExperiment.java
+++ src/ca/ilanguage/oprime/bilingualaphasiatest/ui/VideoRecorderSubExperiment.java
@@ -1,27 +1,45 @@
 package ca.ilanguage.oprime.bilingualaphasiatest.ui;
 
-import java.io.File;
 
 import ca.ilanguage.oprime.bilingualaphasiatest.R;
+
+
+import java.io.File;
 import android.app.Activity;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.MediaController;
 import android.widget.VideoView;
 
+/**
+ * Android video recorder with "no" preview (the preview is a 1x1 pixel which 
+ * simulates an unobtrusive recording led). Based on Pro Android 2 2010 (Hashimi et al) 
+ * source code in Listing 9-6. 
+ * 
+ * Suitable use cases:
+ *  A: eye gaze tracking to let users use eyes to navigate a page
+ *  B: use tablet camera(s) to replace video camera in lab experiments 
+ *     (psycholingusitics or other experiments)
+ * 
+ * Video is recording is controlled in two ways:
+ * 1. Video starts and stops with the activity
+ * 2. Video starts and stops on any touch
+ * 
+ * To control recording in other ways see the try blocks of the onTouchEvent 
+ * 
+ * @author cesine
+ *
+ */
 public class VideoRecorderSubExperiment extends Activity implements
 		SurfaceHolder.Callback {
 
 	private MediaRecorder recorder = null;
-	private static final String OUTPUT_FILE = "/sdcard/videooutput.mp4";
+	private static final String OUTPUT_FILE = "/sdcard/videooutput";
 	private static final String TAG = "RecordVideo";
 	private VideoView videoView = null;
-	private Button startBtn = null;
+	private Boolean mRecording = false;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -29,34 +47,60 @@ public class VideoRecorderSubExperiment extends Activity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.video_recorder);
 
-		startBtn = (Button) findViewById(R.id.bgnBtn);
-
-		
 		videoView = (VideoView) this.findViewById(R.id.videoView);
 
 		final SurfaceHolder holder = videoView.getHolder();
 		holder.addCallback(this);
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-		startBtn.setOnClickListener(new OnClickListener() {
+	}
 
-			@Override
-			public void onClick(View view) {
+	public boolean onTouchEvent(MotionEvent event) {
+		// can use the xy of the touch to start and stop recording
+		float positionX = event.getX();
+		float positionY = event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			// Screen is pressed for the first time
+			break;
+		case MotionEvent.ACTION_MOVE:
+			// Screen is still pressed, float have been updated
+			break;
+		case MotionEvent.ACTION_UP:
+			// Screen is not touched anymore
+			if (mRecording) {
+				// To stop recording attach this try block to another event listener,
+				// button etc
 				try {
-					beginRecording(holder);
+					stopRecording();
+				} catch (Exception e) {
+					Log.e(TAG, e.toString());
+					e.printStackTrace();
+				}
+			} else {
+				// To begin recording attach this try block to another event listener,
+				// button etc
+				try {
+					beginRecording(videoView.getHolder());
 				} catch (Exception e) {
 					Log.e(TAG, e.toString());
 					e.printStackTrace();
 				}
 			}
-		});
-
-	
+			break;
+		}
+		return super.onTouchEvent(event);
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		startBtn.setEnabled(true);
+		try {
+			beginRecording(holder);
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -70,8 +114,11 @@ public class VideoRecorderSubExperiment extends Activity implements
 	}
 
 	private void stopRecording() throws Exception {
+		mRecording = false;
 		if (recorder != null) {
 			recorder.stop();
+			recorder.release();
+			recorder = null;
 		}
 	}
 
@@ -84,18 +131,18 @@ public class VideoRecorderSubExperiment extends Activity implements
 			e.printStackTrace();
 		}
 		super.onDestroy();
-		if (recorder != null) {
-			recorder.release();
-		}
+
 	}
 
 	private void beginRecording(SurfaceHolder holder) throws Exception {
 		if (recorder != null) {
 			recorder.stop();
 			recorder.release();
+			recorder = null;
 		}
 
-		File outFile = new File(OUTPUT_FILE);
+		String uniqueOutFile = OUTPUT_FILE + System.currentTimeMillis() + ".mp4";
+		File outFile = new File(uniqueOutFile);
 		if (outFile.exists()) {
 			outFile.delete();
 		}
@@ -105,16 +152,17 @@ public class VideoRecorderSubExperiment extends Activity implements
 			recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
 			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 			recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-			recorder.setVideoSize(640, 480);// youtube reccomended size 320x240,
-																			// OpenGazer: 640x480 mode.
+			recorder.setVideoSize(640, 480);// YouTube recommended size 320x240,
+																			// OpenGazer eye tracker: 640x480 mode.
 			recorder.setVideoFrameRate(15);
 			recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
 			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 			recorder.setMaxDuration(30000); // limit to 30 seconds
 			recorder.setPreviewDisplay(holder.getSurface());
-			recorder.setOutputFile(OUTPUT_FILE);
+			recorder.setOutputFile(uniqueOutFile);
 			recorder.prepare();
 			recorder.start();
+			mRecording = true;
 		} catch (Exception e) {
 			Log.e(TAG, e.toString());
 			e.printStackTrace();
