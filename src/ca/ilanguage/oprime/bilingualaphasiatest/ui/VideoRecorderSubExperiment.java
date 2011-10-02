0878e33f665386245f92f8d9fac69b2c66902894
cesine <cesine@host>
2011-10-02 10:29:31 -0400

    removing buttons and reducing preview to 1 pixel by 1 pixel so it acts like a led to indicate recording

--- src/ca/ilanguage/oprime/bilingualaphasiatest/ui/VideoRecorderSubExperiment.java
+++ src/ca/ilanguage/oprime/bilingualaphasiatest/ui/VideoRecorderSubExperiment.java
@@ -14,161 +14,110 @@ import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+public class VideoRecorderSubExperiment extends Activity implements
+		SurfaceHolder.Callback {
 
-public class VideoRecorderSubExperiment extends Activity implements SurfaceHolder.Callback {
+	private MediaRecorder recorder = null;
+	private static final String OUTPUT_FILE = "/sdcard/videooutput.mp4";
+	private static final String TAG = "RecordVideo";
+	private VideoView videoView = null;
+	private Button startBtn = null;
 
-	    private MediaRecorder recorder = null;
-	    private static final String OUTPUT_FILE = "/sdcard/videooutput.mp4";
-	    private static final String TAG = "RecordVideo";
-	    private VideoView videoView = null;
-	    private Button startBtn = null;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.video_recorder);
+
+		startBtn = (Button) findViewById(R.id.bgnBtn);
+
+		
+		videoView = (VideoView) this.findViewById(R.id.videoView);
+
+		final SurfaceHolder holder = videoView.getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+		startBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				try {
+					beginRecording(holder);
+				} catch (Exception e) {
+					Log.e(TAG, e.toString());
+					e.printStackTrace();
+				}
+			}
+		});
+
 	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.video_recorder);
+	}
 
-        startBtn = (Button) findViewById(R.id.bgnBtn);
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		startBtn.setEnabled(true);
+	}
 
-        Button endBtn = (Button) findViewById(R.id.stpBtn);
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+	}
 
-        Button playRecordingBtn = (Button) findViewById(R.id.playRecordingBtn);
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		Log.v(TAG, "Width x Height = " + width + "x" + height);
+	}
 
-        Button stpPlayingRecordingBtn = 
-(Button) findViewById(R.id.stpPlayingRecordingBtn);
+	private void stopRecording() throws Exception {
+		if (recorder != null) {
+			recorder.stop();
+		}
+	}
 
-        videoView = (VideoView)this.findViewById(R.id.videoView);
+	@Override
+	protected void onDestroy() {
+		try {
+			stopRecording();
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			e.printStackTrace();
+		}
+		super.onDestroy();
+		if (recorder != null) {
+			recorder.release();
+		}
+	}
 
-        final SurfaceHolder holder = videoView.getHolder();
-        holder.addCallback(this);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	private void beginRecording(SurfaceHolder holder) throws Exception {
+		if (recorder != null) {
+			recorder.stop();
+			recorder.release();
+		}
 
-        startBtn.setOnClickListener(new OnClickListener() {
+		File outFile = new File(OUTPUT_FILE);
+		if (outFile.exists()) {
+			outFile.delete();
+		}
 
-            @Override
-            public void onClick(View view) {
-                try {
-                    beginRecording(holder);
-                } catch (Exception e) {
-                    Log.e(TAG, e.toString());
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        endBtn.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                try {
-                    stopRecording();
-                } catch (Exception e) {
-                    Log.e(TAG, e.toString());
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        playRecordingBtn.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                try {
-                    playRecording();
-                } catch (Exception e) {
-                    Log.e(TAG, e.toString());
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        stpPlayingRecordingBtn.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                try {
-                    stopPlayingRecording();
-                } catch (Exception e) {
-                    Log.e(TAG, e.toString());
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        startBtn.setEnabled(true);
-    }
-    
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-	    	    int height) {
-        Log.v(TAG, "Width x Height = " + width + "x" + height);
-    }
-
-    private void playRecording() {
-        MediaController mc = new MediaController(this);
-        videoView.setMediaController(mc);
-        videoView.setVideoPath(OUTPUT_FILE);
-        videoView.start();
-    }
-    
-    private void stopPlayingRecording() {
-        videoView.stopPlayback();
-    }
-    
-    private void stopRecording() throws Exception {
-        if (recorder != null) {
-            recorder.stop();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (recorder != null) {
-            recorder.release();
-        }
-    }
-
-    private void beginRecording(SurfaceHolder holder) throws Exception {
-        if(recorder!=null)
-        {
-            recorder.stop();
-            recorder.release();
-        }
-
-        File outFile = new File(OUTPUT_FILE);
-        if(outFile.exists())
-        {
-            outFile.delete();
-        }
-
-        try {
-            recorder = new MediaRecorder();
-            recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            recorder.setVideoSize(176, 144);
-            recorder.setVideoFrameRate(15);
-            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            recorder.setMaxDuration(30000); // limit to 30 seconds
-            recorder.setPreviewDisplay(holder.getSurface());
-            recorder.setOutputFile(OUTPUT_FILE);
-            recorder.prepare();
-            recorder.start();
-        }
-        catch(Exception e) {
-            Log.e(TAG, e.toString());
-            e.printStackTrace();
-        }
-    }
+		try {
+			recorder = new MediaRecorder();
+			recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+			recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+			recorder.setVideoSize(640, 480);// youtube reccomended size 320x240,
+																			// OpenGazer: 640x480 mode.
+			recorder.setVideoFrameRate(15);
+			recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+			recorder.setMaxDuration(30000); // limit to 30 seconds
+			recorder.setPreviewDisplay(holder.getSurface());
+			recorder.setOutputFile(OUTPUT_FILE);
+			recorder.prepare();
+			recorder.start();
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			e.printStackTrace();
+		}
+	}
 }
-
